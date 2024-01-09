2623991c1caa141dc778bc58b8c43b5cac18411b
qweroot <qweroot@pm.me>
2024-01-10 07:10:40 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div class="mx-auto max-w-lg py-2">
+      <p class="font-bold text-zinc-800">Init commit</p>
+    </div>
+  </main>
+</template>
